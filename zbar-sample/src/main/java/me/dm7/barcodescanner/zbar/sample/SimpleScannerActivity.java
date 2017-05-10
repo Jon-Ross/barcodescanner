3da2b8dfb18a8af07b358dc5442fd00fe62bf9e1
Jon-Ross <jon-ross.rodrigues@hotmail.co.uk>
2017-05-10 16:09:28 +0100

    added pause and resume into simple scanner activity

--- zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/SimpleScannerActivity.java
+++ zbar-sample/src/main/java/me/dm7/barcodescanner/zbar/sample/SimpleScannerActivity.java
@@ -2,14 +2,19 @@ package me.dm7.barcodescanner.zbar.sample;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import me.dm7.barcodescanner.zbar.Result;
 import me.dm7.barcodescanner.zbar.ZBarScannerView;
 
 public class SimpleScannerActivity extends BaseScannerActivity implements ZBarScannerView.ResultHandler {
+
     private ZBarScannerView mScannerView;
+    private boolean paused;
+    private TextView pauseResume;
 
     @Override
     public void onCreate(Bundle state) {
@@ -19,6 +24,31 @@ public class SimpleScannerActivity extends BaseScannerActivity implements ZBarSc
         ViewGroup contentFrame = (ViewGroup) findViewById(R.id.content_frame);
         mScannerView = new ZBarScannerView(this);
         contentFrame.addView(mScannerView);
+
+        paused = false;
+        pauseResume = (TextView) findViewById(R.id.pause_resume);
+        pauseResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(paused) {
+                    startCamera();
+                } else {
+                    stopCamera();
+                }
+            }
+        });
+    }
+
+    private void startCamera() {
+        paused = false;
+        mScannerView.startCamera();
+        pauseResume.setText("PAUSE");
+    }
+
+    private void stopCamera() {
+        paused = true;
+        mScannerView.stopCamera();
+        pauseResume.setText("RESUME");
     }
 
     @Override
